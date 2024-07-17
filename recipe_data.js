c50e2c2dd197ae953d229ff56aefb45ddc8d892d
Anita Ababio <gibbsonanita@gmail.com>
2024-07-17 21:28:06 +0000

    Update recipe_data.js

---
+++ recipe_data.js
@@ -0,0 +1,12 @@
+const recipes = `
+Food ,Ingredients,Directions,Image
+Chocolate Cake,"1 (18.25 ounce) package devil's food cake mix
+1 (5.9 ounce) package instant chocolate pudding mix
+1 cup sour cream
+1 cup vegetable oil
+4 large eggs eggs
+½ cup warm water
+2 cups semisweet chocolate chips  ","Preheat oven to 350 degrees F (175 degrees C).
+In a large bowl, mix together the cake and pudding mixes, sour cream, oil, beaten eggs and water. Stir in the chocolate chips and pour batter into a well greased 12 cup bundt pan.
+Bake for 50 to 55 minutes, or until top is springy to the touch and a wooden toothpick inserted comes out clean. Cool cake thoroughly in pan at least an hour and a half before inverting onto a plate If desired, dust the cake with powdered sugar.",https://www.cookingclassy.com/wp-content/uploads/2019/10/chocolate-cake-3.jpg
+`
